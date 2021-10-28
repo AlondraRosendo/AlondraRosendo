8c837a51a5ca8762c31489a975aa965003f8bd64
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-28 17:45:48 -0500

    Create Función 5.m

---
+++ Función 5.m
@@ -0,0 +1,13 @@
+%Octave Script
+%Title:  Funcion real de variable real
+%Descripcion:  Scriptpara graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  27-10-2021
+%Funcion:  5
+%ISIC:  3101
+
+clear
+z=-130:0.1:130;
+gz=(z.^3);
+plot(z,gz)
