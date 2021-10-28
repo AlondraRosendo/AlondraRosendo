b5ee2703db91e0539f7c8422526e1ce808c5cb25
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-28 17:47:16 -0500

    Create Función 6.m

---
+++ Función 6.m
@@ -0,0 +1,14 @@
+%Octave Script
+%Title:  Funcion real de variable real
+%Descripcion:  Scriptpara graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  27-10-2021
+%Funcion:  6
+%ISIC:    3101
+
+clear
+t=-7:0.1:7;
+ht=(t-1)./(t-2);
+plot(t,ht)
+
