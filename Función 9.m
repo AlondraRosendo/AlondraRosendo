70a58202e6872dd6832068b89df444a6ec66e298
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-28 17:51:33 -0500

    Create Función 9.m

---
+++ Función 9.m
@@ -0,0 +1,19 @@
+%Octave Script
+%Title:  Funcion real de variable real
+%Descripcion:  Scriptpara graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  27-10-2021
+%Funcion:  9
+%ISIC:  3101
+
+%La funcion f, con dominio en todo R
+%Calcula f(-1),f(5)yf(6)
+f=@(x)(-x+2).*(x<-1)+(x).*(-1<=x).*(x<=2)+(2).*(x>2);
+f(-1)
+f(5)
+f(6)
+%Estudia la continuidad de f
+f(3)
+f(5)
+fplot(@(x)(-x+2).*(x<-1)+(x).*(-1<=x).*(x<=2)+(2).*(x>2),[-5,5])
