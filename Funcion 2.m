d7838ab1d6f0163ac8cc7893b3a9c8dbf12e0fd0
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-28 17:40:53 -0500

    Create Funcion 2.m

---
+++ Funcion 2.m
@@ -0,0 +1,12 @@
+%Octave Script
+%Title:  Funcion real de variable real
+%Descripcion:  Script para graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  27-10-2021
+%Funcion:  2
+
+clear
+x=-5:1:5;
+fx=1+x.^2;
+plot(x,fx)
