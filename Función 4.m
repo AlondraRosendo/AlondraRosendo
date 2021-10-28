d794859bf2cfe9da347e57a244fb2d7fc9c63904
AlondraRosendo <91708278+AlondraRosendo@users.noreply.github.com>
2021-10-28 17:44:21 -0500

    Create Función 4.m

---
+++ Función 4.m
@@ -0,0 +1,13 @@
+%Octave Script
+%Title:  Funcion real de variable real
+%Descripcion:  Scriptpara graficar una funcion
+%Autora:  Alondra Cayetano Rosendo
+%Escuela:  Tecnologico de Estudios Superiores de Jilotepec
+%Date:  27-10-2021
+%Funcion:  4
+%ISIC:  3101
+
+clear
+x=-55:0.1:55;
+fx=(x.^2)+(6*(x));
+plot(x,fx)
